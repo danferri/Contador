8c425d694323ac9e7c576b0ed7156e9a5e502b70
dan <d.ferri@aluno.ifsp.edu.br>
2024-08-19 17:06:38 -0300

    ajustes

--- app/src/main/java/br/edu/ifsp/scl/ads/contador/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/contador/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.ViewParent
+import android.widget.Adapter
+import android.widget.AdapterView
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -12,6 +15,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.AdapterView.OnItemSelectedListener
 import br.edu.ifsp.scl.ads.contador.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -24,28 +28,40 @@ class MainActivity : AppCompatActivity() {
         amb = ActivityMainBinding.inflate(layoutInflater)
         setContentView(amb.root)
 
-        amb.zeroRb.setOnClickListener{
-           contador = amb.zeroRb.text.toString().toInt()
+        amb.inicialSp.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent:AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                contador = when(position){
+                    0 -> 0
+                    1 -> 5
+                    2 -> 10
+                    else -> 0 //Esgotar as opções quando when é usado como expression.
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?){
+                //NSA
+            }
         }
 
-        amb.dezRb.setOnClickListener {
-            contador = 10
-        }
-
-        /*amb.cliqueBt.setOnClickListener {
+        amb.cliqueBt.setOnClickListener {
             amb.contadorTv.text = ((++contador).toString())
         }
 
-        cliqueBt.setOnClickListener(object: View.OnClickListener {
+        /*cliqueBt.setOnClickListener(object: View.OnClickListener {
             override fun onClick(v: View?) {
                 Toast.makeText(this@MainActivity, "vc clicou!", Toast.LENGTH_SHORT).show()
             }
-        })*/
+        })
 
         amb.cliqueBt.setOnClickListener {
             Toast.makeText(this@MainActivity, "vc clicou!", Toast.LENGTH_SHORT).show()
             amb.contadorTv.text = ((++contador).toString())
-        }
+        }*/
 
     }
 }
